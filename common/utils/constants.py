2e21742117be2fa85cc9fb5714aed7103922edab
Yangchen <a2045751@163.com>
2021-01-22 02:44:55 +0800

    flask env config flask环境配置信息的说明

--- common/utils/constants.py
+++ common/utils/constants.py
@@ -1,2 +1,17 @@
 # 配置文件环境变量名
 GLOBAL_SETTING_ENV_NAME = 'BIGEVENT_WEB_SETTINGS'
+
+# ### 开发模式
+#
+# ```shell
+# export FLASK_ENV=development
+# export BIGEVENT_WEB_SETTINGS=/path/to/config/file
+# ```
+#
+# ### 线上模式
+#
+# ```shell
+# export FLASK_ENV=production
+# export BIGEVENT_WEB_SETTINGS=/path/to/config/file
+# export BIGEVENT_CELERY_SETTINGS=/path/to/config/file
+# ```
